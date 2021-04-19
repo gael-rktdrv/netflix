c9675f4e0cdf41ab86f11429affbeadaf8a23535
gael-rktdrv <max.kzntsv@yandex.ru>
2021-04-19 04:22:27 +0200

    updates

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ import common
 import naive_em
 import em
 
-X = np.loadtxt("toy_data.txt")
 
-# TODO: Your code here
+def main():
+    X = np.loadtxt("toy_data.txt")
+    K, seeds, res = np.array([1,2,3,4]), list(range(5)), {}
+
+    for seed in seeds:
+        # import pdb; pdb.set_trace()
+        mixtures = []
+        for k in K:
+            mixture, post = common.init(X, k, seed)
+            mixtures.append(mixture)
+        res[seed] = mixtures
+
+    for key, value in res.items():
+        print(f"Seed {key}:\n{value}")
+
+
+if __name__=="__main__":
+    main()
