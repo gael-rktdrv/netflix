5ebfa2190fcc415858a71c9ab6d937d830159f54
gael-rktdrv <max.kzntsv@yandex.ru>
2021-04-21 13:38:25 +0200

    Main update

--- main.py
+++ main.py
@@ -4,42 +4,80 @@ import common
 import naive_em
 import em
 import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 
-def kmeans(X, Ks, seeds):
+def km(X, Ks, seeds):
     costs = {}
     
     for K in Ks:
+        # fig = make_subplots(rows=2, cols=2)
+        # a, b = 1, 1
         temp = []
         for seed in seeds:
             mixture, post = common.init(X, K=K, seed=seed)
             mixture, post, cost = kmeans.run(X, mixture, post)
-            common.plot(X=X, mixture=mixture, post=post, title=f'K=1 | seed={seed} | cost={cost:.3f}')
+            title = f'K={K} | seed={seed} | cost={cost:.3f}'
+            # Plotting all 4 seeds
+            # fig.add_trace(
+            #     common.plot(X=X, mixture=mixture, post=post, title=title), 
+            #     row=a, col=b
+            #     )
+            common.plot(X=X, mixture=mixture, post=post, title=title)
             plt.show()
             temp.append(round(cost, 3))
+            # Managing rows and columns
+            # if a < 2:
+            #     a += 1
+            # else:
+            #     b += 1
+        # fig.update_layout(height=600, width=800, title_text=f"K={K}")
+        # fig.show()
+
         costs[K] = min(temp)
     print(costs)
 
 
-def naive_em(X, Ks, seeds):
-    costs = {}
+def n_em(X, Ks, seeds):
+    LL = {}
     
     for K in Ks:
+        # fig = make_subplots(rows=2, cols=2)
+        # a, b = 1, 1
         temp = []
         for seed in seeds:
             mixture, post = common.init(X, K=K, seed=seed)
-            mixture, post, _, cost = naive_em.run(X, mixture, post)
-            common.plot(X=X, mixture=mixture, post=post, title=f'K=1 | seed={seed} | cost={cost:.3f}')
+            mixture, post, ll, _ = naive_em.run(X, mixture)
+            title = f'K={K} | seed={seed} | likelihood={ll:.3f}'
+            # Plotting all 4 seeds
+            # fig.add_trace(
+            #     common.plot(X=X, mixture=mixture, post=post, title=title), 
+            #     row=a, col=b
+            #     )
+            common.plot(X=X, mixture=mixture, post=post, title=title)
             plt.show()
-            temp.append(round(cost, 3))
-        costs[K] = min(temp)
-    print(costs)
+            temp.append(round(ll, 3))
+            # Managing rows and columns
+            # if a < 2:
+            #     a += 1
+            # else:
+            #     b += 1
+        # fig.update_layout(height=600, width=800, title_text=f"K={K}")
+        # fig.show()
+        LL[K] = min(temp)
+    print(f"Likelihoods: \n{LL}")
 
 
 def main():
     X = np.loadtxt("toy_data.txt")
     Ks, seeds = np.array([1,2,3,4]), list(range(5))
-    naive_em(X, Ks, seeds):
+    # print('Choose method: \n1-Kmeans\n2-EM algorithm')
+    choice = int(input("Choose method: \n1-Kmeans\n2-EM algorithm\nChoice: "))
+    if choice == 1:
+        km(X, Ks, seeds)  # Running Kmeans
+    else:
+        n_em(X, Ks, seeds) # Running naive_em
 
 
 if __name__=="__main__":
